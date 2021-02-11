14b04577184772b03fc5d814701738f9f946f29f
AvyChanna <30595749+AvyChanna@users.noreply.github.com>
2021-02-11 15:54:10 +0530

    MD5 collisions

--- myutils/__init__.py
+++ myutils/__init__.py
@@ -16,16 +16,17 @@ import time
 from base64 import b64decode, b64encode
 from collections import Counter, namedtuple
 from functools import reduce
+from hashlib import md5, sha1, sha256
 from itertools import cycle, islice
 from json import dumps as pp
 from multiprocessing import Pool
-from textwrap import wrap
-from typing import List
+from textwrap import wrap, dedent
 
 import gmpy2 as gmp
 import labmath as lm
 import requests
 
+from . import collisions
 from .conversion import a2b, a2h, a2i, b2a, b2h, b2i, convert, h2a, h2b, h2i, i2a, i2b, i2h, norm_hex
 from .ec import ec_curve, ec_point
 from .multicomp import run_cmd, run_with_timeout, worker
---
+++ myutils/collisions.py
@@ -0,0 +1,14 @@
+md5coll = [
+    (
+        bytes.fromhex(
+            '4dc968ff0ee35c209572d4777b721587d36fa7b21bdc56b74a3dc0783e7b9518afbfa200a8284bf36e8e4b55b35f427593d849676da0d1555d8360fb5f07fea2'),
+        bytes.fromhex(
+            '4dc968ff0ee35c209572d4777b721587d36fa7b21bdc56b74a3dc0783e7b9518afbfa202a8284bf36e8e4b55b35f427593d849676da0d1d55d8360fb5f07fea2'),
+    ),
+    (
+        bytes.fromhex(
+            "0e306561559aa787d00bc6f70bbdfe3404cf03659e704f8534c00ffb659c4c8740cc942feb2da115a3f4155cbb8607497386656d7d1f34a42059d78f5a8dd1ef"),
+        bytes.fromhex(
+            "0e306561559aa787d00bc6f70bbdfe3404cf03659e744f8534c00ffb659c4c8740cc942feb2da115a3f415dcbb8607497386656d7d1f34a42059d78f5a8dd1ef"),
+    ),
+]
