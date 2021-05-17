9ba1be7614dad4c34943b67cbbe8e844d898b52e
Avneet Singh <30595749+AvyChanna@users.noreply.github.com>
2021-05-17 20:33:09 +0530

    Update misc.py

--- myutils/misc.py
+++ myutils/misc.py
@@ -2,4 +2,10 @@ import functools
 import operator
 
 def bxor(*inputs):
-	return bytes(i^j for i,j in zip(*inputs))
+	return bytes(functools.reduce(lambda x,y:x^y, i) for i in zip(*inputs))
+
+def blockize(data, k=16):
+	return [data[i:i+k] for i in range(0, len(data), k)]
+def wr(data):
+	with open("a.txt", "wb") as f:
+		f.write(data)
